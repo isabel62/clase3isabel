15601571a59b92633f9c018b488d72c624ab6cdc
isabel62 <isabeliribarreng@hotmail.com>
2017-10-26 19:44:22 -0300

    Creaciopn clase con estrustura completa atrivuto construstor y

---
+++ src/clase3isabel/caballo.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase3isabel;
+
+/**
+ *
+ * @author LAB08
+ */
+public class caballo {
+    String altura;
+    String color;
+    String peso;
+    String rasa;
+    int patas= 4, edad;
+    
+    //Metodo constructor
+    public Caballo(String nuevoNombre)
+    {
+    raza = nuevoNombre;
+}
+//Metodo get y set de la clase
+    public relinchar()
+    {
+        
+        
+   }
+    //Metodo para obtener la raza del caballo
+    public String obtenerRaza()
+        {
+        //Sentencia obligatoria de retorno
+        return raza;    
+        }
+         public int obtenerEdad()
+         {
+         //Sentencia obligatoria de retorno
+          return edad;
+           
+         }
+        
+            
+       
+    }
